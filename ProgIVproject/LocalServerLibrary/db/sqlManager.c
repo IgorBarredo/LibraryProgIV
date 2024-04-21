8c6069622b5aa615e721e0adbdf6edbaa58741a1
Iker Alvis <iker.a@opendeusto.es>
2024-04-21 14:23:10 +0200

    Funciones para la conexion con la base de datos

--- ProgIVproject/LocalServerLibrary/db/sqlManager.c
+++ ProgIVproject/LocalServerLibrary/db/sqlManager.c
@@ -1,33 +1,117 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "autor.h"
+#include "categoria.h"
+#include "editorial.h"
 #include "sqlite3.h"
 
 sqlite3* abrirDB() {
     sqlite3 *db;
     int result = sqlite3_open("biblioteca.db", &db);
     if (result != SQLITE_OK) {
-		printf("Error opening database\n");
-	}
+        printf("Error opening database\n");
+        return NULL;
+    }
     return db;
 }
-
-/*int anadirAutor(){
-    sqlite3 *db = abrirDB();
-    int resultado;
-    sqlite3_stmt *stmt;
-    char sql1[]= "insert into Autor (id_aut, nombre_a, fecha_ncto, lugar_ncto) values (null, ?, ?, ?)";
-    sqlite3_prepare_v2(db, sql1, strlen(sql1) + 1, &stmt, NULL) ;
-	sqlite3_bind_text(stmt, 1, nombre, strlen(nombre), SQLITE_STATIC);
-	sqlite3_bind_int(stmt, 2, campeon);
+void cerrarDB(sqlite3 *db) {
+    sqlite3_close(db);
 }
 
-char sql[100];
-sprintf(sql,"insert into Autor (id_aut, nombre_a, fecha_ncto, lugar_ncto) values (null, '%s', '%s', '%s')", name, date, place); 
 
-	int rc = sqlite3_exec(db, sql, 0, 0, 0);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Error al insertar autor: %s\n", sqlite3_errmsg(db));
+
+void insertarAutor(Autor objAutor) {
+    sqlite3 *db = abrirDB();
+    int result;
+
+    sqlite3_stmt *stmt;
+	char sql[] = "INSERT INTO autor (nombre_a, fecha_ncto, lugar_ncto) VALUES (?, ?, ?)";
+
+	result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL);
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement: %s\n", sqlite3_errmsg(db));
+		guardarErrorEnLog("Error preparing statement");
+		sqlite3_close(db);
+		return;
+	}
+
+	sqlite3_bind_text(stmt, 1, objAutor.name, strlen(objAutor.name), SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, objAutor.date, strlen(objAutor.date), SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 3, objAutor.place, strlen(objAutor.place), SQLITE_STATIC);
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) {
+		printf("Error inserting data: %s\n", sqlite3_errmsg(db));
+		guardarErrorEnLog("Error inserting data");
+	}
+
+	cerrarDB(db);
+
+    
+}
+
+void insertarCategoria(Categoria objCategoria) {
+    sqlite3 *db = abrirDB();
+    int result;
+
+    sqlite3_stmt *stmt;
+    char sql[] = "INSERT INTO categoria (nombre_c) VALUES (?)";
+
+    result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL);
+    if (result != SQLITE_OK) {
+        printf("Error preparing statement: %s\n", sqlite3_errmsg(db));
+        guardarErrorEnLog("Error preparing statement");
+        sqlite3_close(db);
         return;
     }
-*/
+
+    sqlite3_bind_text(stmt, 1, objCategoria.name, strlen(objCategoria.name), SQLITE_STATIC);
+
+    result = sqlite3_step(stmt);
+    if (result != SQLITE_DONE) {
+        printf("Error inserting data: %s\n", sqlite3_errmsg(db));
+        guardarErrorEnLog("Error inserting data");
+    }
+    sqlite3_finalize(stmt);
+    cerrarDB(db);
+}
+
+void insertarEditorial(Editorial objEditorial) {
+    sqlite3 *db = abrirDB();
+    int result;
+
+    sqlite3_stmt *stmt;
+    char sql[] = "INSERT INTO editorial (nombre_e, fecha_fundacion) VALUES (?, ?)";
+
+    result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL);
+    if (result != SQLITE_OK) {
+        printf("Error preparing statement: %s\n", sqlite3_errmsg(db));
+        guardarErrorEnLog("Error preparing statement");
+        sqlite3_close(db);
+        return;
+    }
+
+    sqlite3_bind_text(stmt, 1, objEditorial.nombre, strlen(objEditorial.nombre), SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, objEditorial.fecha, strlen(objEditorial.fecha), SQLITE_STATIC);
+
+    result = sqlite3_step(stmt);
+    if (result != SQLITE_DONE) {
+        printf("Error inserting data: %s\n", sqlite3_errmsg(db));
+        guardarErrorEnLog("Error inserting data");
+    }
+    
+    sqlite3_finalize(stmt);
+    cerrarDB(db);
+}
+
+	
+
+	
+	
+
+	
+
+	
+
+	
